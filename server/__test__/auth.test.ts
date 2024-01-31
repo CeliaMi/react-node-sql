3a163959a97e9f219a2a92edb60e27136cb7cfbb
Celia <celiamillanalemany@gmail.com>
2024-01-31 13:23:22 +0100

    getOneBook test passed

--- server/__test__/auth.test.ts
+++ server/__test__/auth.test.ts
@@ -22,14 +22,24 @@ describe("AUTH api/auth" , ()=>{
             // await UserModel.destroy({where:{ email: testAuthLogin.email}});
           });
 
-    test("should be return 201", async()=> {
+    test("When the user makes a successful registration should be return 201", async()=> {
         const response = await request(app).post('/api/register')
         .send(testAuthRegister)
         expect(response.statusCode).toEqual(201)
         expect(response.body).toHaveProperty("sesiondata")
     })
 
-    test("when user doesn't exist should be return 403", async()=> {
+    test("When the user login with an incorrect password it throws an error PASSWORD_INVALID", async () => {
+        const newTestAuthLogin = {...testAuthRegister, password:"wrong password"}
+        const response = await request(app)
+          .post("/api/login")
+          .send(newTestAuthLogin);
+        expect(response.statusCode).toEqual(401);
+        expect(response.body.error).toContain("PASSWORD_INVALID")
+       
+      });
+
+    test("when user doesn't exist throws an error USER_NOT_EXISTS", async()=> {
         const response = await request(app).post('/api/login')
         .send(UnregisteredUser)
         expect(response.statusCode).toEqual(404)
--- server/__test__/book.test.ts
+++ server/__test__/book.test.ts
@@ -5,60 +5,94 @@ import FileModel from '../models/FileModel'
 import { tokenSign } from '../utils/handlejwt'
 import db from '../database/db'
 import BookModel from '../models/BookModel'
-describe("Book api/book" , ()=>{
 
-let FILE_URL = "";
-let JWT_TOKEN = "";
-let USER_ID = "";
+describe("CRUD api/book" , ()=>{
 
-const testAuthRegisterAdmin = {
-    "name":"userTest",
-    "email": "test@booktest15.com",
-    "password": "123456789",
-    "role": "admin"
-}
-const testFile ={
-    url: "https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    filename: "file-test.jpg",
-}
+  let FILE_URL = "";
+  let JWT_TOKEN = "";
+  let USER_ID = "";
 
-const testBookData = {
-        title: "test title",
-        writer: "test writer",
-        book_description: "test description",
-  };
+  const testAuthRegisterAdmin = {
+      "name":"userTest",
+      "email": "test@booktest15.com",
+      "password": "123456789",
+      "role": "admin"
+  }
+  const testFile ={
+      url: "https://images.unsplash.com/photo-1610116306796-6fea9f4fae38?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      filename: "file-test.jpg",
+  }
+
+  const testBookData = {
+          title: "test title",
+          writer: "test writer",
+          book_description: "test description",
+    };
 
 
-beforeAll(async () => {
-   await db.authenticate()
-  // await UserModel.destroy({ where: { id: USER_ID } })
-  // await FileModel.destroy({ where: { id_user: USER_ID }})
-    const user = await UserModel.create(testAuthRegisterAdmin);
-    USER_ID = await  user?.get('id')?.toString() as string;
-    const testFileRegister = {  ...testFile, 
-        id_user: USER_ID };
-    const file = await FileModel.create(testFileRegister);
-    FILE_URL = await file?.get('url')?.toString() as string; 
-    JWT_TOKEN = await tokenSign(user);
-  });
+  beforeAll(async () => {
+    await db.authenticate()
+      const user = await UserModel.create(testAuthRegisterAdmin);
+      USER_ID = await  user?.get('id')?.toString() as string;
+      const testFileRegister = {  ...testFile, 
+          id_user: USER_ID };
+      const file = await FileModel.create(testFileRegister);
+      FILE_URL = await file?.get('url')?.toString() as string; 
+      JWT_TOKEN = await tokenSign(user);
+    });
 
-  test("should be register book", async () => {
+  describe( 'POST /books', ()=> {
+  test("When the user enters a book it should return a success message and the data", async () => {
     const dataNewBook = { 
       ...testBookData, 
       file_url: FILE_URL ,
     };
-  
+    const response = await request(app)
+      .post("/api/books")
+      .set("Authorization", `Bearer ${JWT_TOKEN}`)
+      .send(dataNewBook);
+    const body = response.body;
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.message).toContain( "has been created successfully!")
+    expect(body).toHaveProperty("body.book_description");  
+  });
+
+
+  test("When the user enters a book it has to be automatically associated with their user ID", async () => {
+    const dataNewBook = { 
+      ...testBookData, 
+      file_url: FILE_URL ,
+    };
     const res = await request(app)
       .post("/api/books")
       .set("Authorization", `Bearer ${JWT_TOKEN}`)
       .send(dataNewBook);
-    // const body = res.body;
-    expect(res.statusCode).toEqual(201);
-    // expect(body).toHaveProperty("data");
-    // expect(body).toHaveProperty("data.name");
-    // expect(body).toHaveProperty("data.artist");
-    // expect(body).toHaveProperty("data.cover");
+    const body = res.body;
+    expect(body).toHaveProperty("body.id_user");  
   });
+  })
+
+  describe( 'GET /books', ()=>{
+
+    test('When the user makes a get request it returns a list of books', async ()=>{
+        const response = await request(app)
+        .get("/api/books")
+        .set("Authorization", `Bearer ${JWT_TOKEN}`)
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.books).toBeInstanceOf(Array);
+    })
+
+    test('When the user makes a get request it with a book id returns one book', async ()=>{
+      const createdBook = await BookModel.findOne({ where: { id_user: USER_ID }})
+      const BOOK_ID = createdBook?.get('id')?.toString()
+      const response = await request(app)
+      .get(`/api/books/${BOOK_ID}`)
+      .set("Authorization", `Bearer ${JWT_TOKEN}`)
+      expect(response.statusCode).toEqual(200);
+  })
+
+
+  })
 
   afterAll( async() => { 
     await BookModel.destroy({ where: { id_user: USER_ID }})
